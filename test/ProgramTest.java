818d6af3e487f1981c19d5f25a75f055f5a69f78
Piotr Tomaszewski <piotrekha15@gmail.com>
2019-08-19 21:09:38 +0200

    implements some example test for Program class

--- test/ProgramTest.java
+++ test/ProgramTest.java
@@ -1,4 +1,61 @@
-package PACKAGE_NAME;
+import model.Interval;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
 
 public class ProgramTest {
+
+    Program program;
+
+    @Before
+    public void generateProgramInstance(){
+        this.program = new Program();
+    }
+
+    @Test
+    public void getResultWithAllDisjointIntervalsTest(){
+        List<Interval> intervalList = List.of(
+                new Interval(1,3),
+                new Interval(4,6),
+                new Interval(7,9)
+        );
+        int result = program.getDisjoinIntervals(intervalList);
+        Assert.assertEquals(3,result);
+    }
+
+    @Test
+    public void getResultWithTwoDisjointIntervalsTest(){
+        List<Interval> intervalList = List.of(
+                new Interval(1,4),
+                new Interval(3,6),
+                new Interval(7,9)
+        );
+        int result = program.getDisjoinIntervals(intervalList);
+        Assert.assertEquals(2,result);
+    }
+
+    @Test
+    public void getResultWithTwoJointIntervalsTest(){
+        List<Interval> intervalList = List.of(
+                new Interval(1,8),
+                new Interval(4,6),
+                new Interval(7,9)
+        );
+        int result = program.getDisjoinIntervals(intervalList);
+        Assert.assertEquals(1,result);
+    }
+
+    @Test
+    public void getResultWhentIntervalIsInsideAnotherTest(){
+        List<Interval> intervalList = List.of(
+                new Interval(1,8),
+                new Interval(4,6)
+        );
+        int result = program.getDisjoinIntervals(intervalList);
+        Assert.assertEquals(1,result);
+    }
+
+
 }
